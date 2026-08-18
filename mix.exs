bdee67ef67b4567351e797a1a81aa7f800d45f04
James Harton <james@harton.dev>
2026-08-18 15:38:34 +1200

    chore: add os_mon

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule MyHiFi.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :runtime_tools],
+      extra_applications: [:logger, :runtime_tools, :os_mon],
       mod: {MyHiFi.Application, []}
     ]
   end
